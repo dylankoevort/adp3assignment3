87c2504b93f6e9c430fb117705c5b6ea56d057e4
Jonathan <HitsLikeTruck@users.noreply.github.com>
2021-06-01 14:29:12 +0200

    Added Course & Department classes.

---
+++ src/main/java/za/ac/cput/Entity/Course.java
@@ -0,0 +1,12 @@
+package za.ac.cput.Entity;
+
+/**
+ * Course.java
+ * Course entity created using builder pattern.
+ * @author Jonathan Kleynhans 217268153
+ * 01 June 2021
+ */
+
+public class Course {
+
+}
