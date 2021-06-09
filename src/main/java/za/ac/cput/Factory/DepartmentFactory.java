b1ecbdaa1de8ec788d78153c3f497de431c015f6
Jonathan <HitsLikeTruck@users.noreply.github.com>
2021-06-09 18:29:58 +0200

    Created DepartmentFactory.java class.

---
+++ src/main/java/za/ac/cput/Factory/DepartmentFactory.java
@@ -0,0 +1,13 @@
+package za.ac.cput.Factory;
+import java.util.UUID;
+import za.ac.cput.Entity.Course;
+
+/**
+ * DepartmentFactory.java
+ * Factory class for Department.java
+ * @author Jonathan Kleynhans 217268153
+ * 09 June 2021
+ */
+
+public class DepartmentFactory {
+}
