c28c1e26352b4241929204a70836ee825efb2c67
Jonathan <HitsLikeTruck@users.noreply.github.com>
2021-06-09 18:28:49 +0200

    Added CourseFactory.java code.

--- src/main/java/za/ac/cput/Factory/CourseFactory.java
+++ src/main/java/za/ac/cput/Factory/CourseFactory.java
@@ -1,5 +1,8 @@
 package za.ac.cput.Factory;
 
+import java.util.UUID;
+import za.ac.cput.Entity.Course;
+
 /**
  * CourseFactory.java
  * Factory class for Course.java
@@ -9,4 +12,21 @@ package za.ac.cput.Factory;
 
 public class CourseFactory {
 
+    public static Course build (String courseCode, String title, int departmentID, int credit, int duration,boolean fullTime){
+
+        if(courseCode.isEmpty() || title.isEmpty() || departmentID <= 0 || credit <= 0 || duration <= 0){
+            return null;
+        }
+
+        String id = UUID.randomUUID().toString();
+
+        return new Course.CourseBuilder()
+                .setCourseCode(courseCode)
+                .setTitle(title)
+                .setDepartmentID(departmentID)
+                .setCredit(credit)
+                .setDuration(duration)
+                .setFullTime(fullTime)
+                .build();
+    }
 }
