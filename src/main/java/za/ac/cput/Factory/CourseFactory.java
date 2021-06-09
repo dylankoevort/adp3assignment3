c4875ea443541709bb54b253d4bceebaccfb7321
Jonathan <HitsLikeTruck@users.noreply.github.com>
2021-06-09 17:16:37 +0200

    Created CourseFactory.java.

---
+++ src/main/java/za/ac/cput/Factory/CourseFactory.java
@@ -0,0 +1,12 @@
+package za.ac.cput.Factory;
+
+/**
+ * CourseFactory.java
+ * Factory class for Course.java
+ * @author Jonathan Kleynhans 217268153
+ * 09 June 2021
+ */
+
+public class CourseFactory {
+
+}
