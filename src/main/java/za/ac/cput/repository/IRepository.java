b4f818e7cedd0ce4e379628a7ccd447db72ce03a
Dylan Koevort <218088159@mycput.ac.za>
2021-07-22 12:45:14 +0200

    Added Examination and Student Repositories (including tests), plus GenericHelper

---
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -0,0 +1,17 @@
+package za.ac.cput.repository;
+
+/**
+ *
+ * IRepository.java
+ * Generic Repository Interface
+ * @author Dylan Koevort 218088159
+ * 19 July 2021
+ *
+ */
+
+public interface IRepository<T, ID> {
+    public T create(T t);
+    public T read(ID id);
+    public T update(T t);
+    public boolean delete(ID id);
+}
